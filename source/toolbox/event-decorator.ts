cf1646a1ca4911a5e3d927297230a83803c44c74
Chase Moskal <chasemoskal@gmail.com>
2019-09-16 19:53:03 -0700

    tweak: remove commented event code

--- source/toolbox/event-decorator.ts
+++ source/toolbox/event-decorator.ts
@@ -1,29 +1,17 @@
 
 import {dashify} from "./dashify.js"
 
-export function dashifyEventName(EventClass: Function) {
-	const dashed = dashify(EventClass.name)
-	const result = /^(.*)-event$/i.exec(dashed)
-	return result ? result[1] : dashed
-}
-
 export type EventDetails<T extends CustomEvent> = T extends CustomEvent<infer D> ? D : never
 
 export type Dispatcher<E extends CustomEvent> = (
 	options?: CustomEventInit<EventDetails<E>>
 ) => void
 
-// export function event<E extends CustomEvent>(
-// 	EventClass: new(...args: any[]) => E,
-// 	name2?: string,
-// ): PropertyDecorator {
-// 	const name = name2 || dashifyEventName(EventClass)
-// 	return (target, key) => {
-// 		target[key] = <any>function(this, options = {}) {
-// 			this.dispatchEvent(new EventClass(name, options))
-// 		}
-// 	}
-// }
+export function dashifyEventName(EventClass: Function) {
+	const dashed = dashify(EventClass.name)
+	const result = /^(.*)-event$/i.exec(dashed)
+	return result ? result[1] : dashed
+}
 
 export function prepareEventDecorator(
 	fallbackOptions: CustomEventInit = {}
