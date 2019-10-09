872b955c49618228448ec4d5b122b2859f76cd6b
Chase Moskal <chasemoskal@gmail.com>
2019-10-09 16:18:05 -0700

    fix: select() context

--- source/toolbox/selects.ts
+++ source/toolbox/selects.ts
@@ -1,11 +1,14 @@
 
-export function select<H extends HTMLElement = HTMLElement>(selector: string, context: any = document): H {
-	return document.querySelector(selector)
+export function select<H extends HTMLElement = HTMLElement>(
+	selector: string,
+	context: any = document
+): H {
+	return context.querySelector(selector)
 }
 
 export function selects<H extends HTMLElement = HTMLElement>(
 	selector: string,
 	context: any = document
 ): H[] {
-	return Array.from(document.querySelectorAll(selector))
+	return Array.from(context.querySelectorAll(selector))
 }
