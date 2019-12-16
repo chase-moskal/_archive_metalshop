0fd56df5dade56a68d2ee29d7f1a60590cd9ea6e
Chase Moskal <chasemoskal@gmail.com>
2019-12-15 23:47:55 -0800

    icons: move icons to system/icons/

--- source/components/paywall-panel.ts
+++ source/components/paywall-panel.ts
@@ -1,15 +1,12 @@
 
-import {html, css, svg, LitElement} from "lit-element"
+import {html, css, LitElement} from "lit-element"
 
+import {star} from "../system/icons.js"
 import {PaywallMode} from "../models/paywall-model.js"
 import {mixinLoadable, LoadableState} from "../framework/mixin-loadable.js"
 import {mixinModelSubscription} from "../framework/mixin-model-subscription.js"
 
 import {PaywallModel} from "../interfaces.js"
-
-const icons = {
-	star: svg`<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"/></svg>`
-}
 
 export class PaywallPanel extends
 	mixinLoadable(
@@ -96,7 +93,7 @@ export class PaywallPanel extends
 
 	private _renderPremium() {return html`
 		<header>
-			<div class="icon">${icons.star}</div>
+			<div class="icon">${star}</div>
 			<h3>You are a premium supporter!</h3>
 		</header>
 		<section>
---
+++ source/system/icons.ts
@@ -0,0 +1,8 @@
+
+import {svg} from "lit-html"
+
+export const silhouette = svg`<svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><path fill-rule="evenodd" d="M12 14.002a.998.998 0 0 1-.998.998H1.001A1 1 0 0 1 0 13.999V13c0-2.633 4-4 4-4s.229-.409 0-1c-.841-.62-.944-1.59-1-4 .173-2.413 1.867-3 3-3s2.827.586 3 3c-.056 2.41-.159 3.38-1 4-.229.59 0 1 0 1s4 1.367 4 4v1.002z"/></svg>`
+
+export const star = svg`<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16"><path fill-rule="evenodd" d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74L14 6z"/></svg>`
+
+export const cancel = svg`<svg xmlns="http://www.w3.org/2000/svg" width="14" height="16" viewBox="0 0 14 16"><path fill-rule="evenodd" d="M7 1C3.14 1 0 4.14 0 8s3.14 7 7 7 7-3.14 7-7-3.14-7-7-7zm0 1.3c1.3 0 2.5.44 3.47 1.17l-8 8A5.755 5.755 0 0 1 1.3 8c0-3.14 2.56-5.7 5.7-5.7zm0 11.41c-1.3 0-2.5-.44-3.47-1.17l8-8c.73.97 1.17 2.17 1.17 3.47 0 3.14-2.56 5.7-5.7 5.7z"/></svg>`
