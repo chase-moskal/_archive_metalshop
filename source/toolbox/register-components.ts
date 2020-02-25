c9d0ae15715e7944474ad87817f1d579c38d1e1e
Chase Moskal <chasemoskal@gmail.com>
2020-02-25 03:59:35 -0800

    update: dependencies

--- source/toolbox/register-components.ts
+++ source/toolbox/register-components.ts
@@ -1,7 +1,9 @@
 
 import {dashify} from "./dashify.js"
 
-export function registerComponents(components: {[name: string]: Function}) {
+export function registerComponents(
+	components: {[name: string]: {new(): HTMLElement}}
+) {
 	for (const [name, component] of Object.entries(components))
 		customElements.define(dashify(name), component)
 }
