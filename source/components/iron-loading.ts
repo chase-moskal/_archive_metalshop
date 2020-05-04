0b8d63a3a5b1195465330ba60796dd3fc7cc3a56
Chase Moskal <chasemoskal@gmail.com>
2020-05-04 07:39:43 -0700

    cleanup: iron loading

--- source/components/iron-loading.ts
+++ source/components/iron-loading.ts
@@ -2,30 +2,57 @@
 import * as loading from "../toolbox/loading.js"
 import {mixinStyles} from "../framework/mixin-styles.js"
 import {styles} from "../components/styles/iron-loading-styles.js"
-import {LitElement, TemplateResult, property, html, css} from "lit-element"
-
+import {LitElement, TemplateResult, property, html} from "lit-element"
 import {spinner as spinnerIcon, error as errorIcon} from "../system/icons.js"
 
 @mixinStyles(styles)
 export class IronLoading<Payload = any> extends LitElement {
-	@property({type: Boolean, reflect: true}) ["initially-hidden"]: boolean
-	@property({type: String}) error: string = "error"
-	@property({type: String}) loading: string = "loading"
-	@property({type: Boolean, reflect: true}) loaded = false
-	@property({type: String, reflect: true}) state: string = "none"
-	private _load = loading.load<Payload>()
-	get load(): loading.Load<Payload> { return this._load }
-	set load(value: loading.Load<Payload>) {
-		if (!value) value = loading.none()
-		this._load = value
-		this.loaded = !!loading.payload(value)
-		this.state = loading.LoadState[value.state].toLowerCase()
-		this.requestUpdate()
-	}
+
+	//
+	// initially-hidden shenanigans
+	//
+
+	@property({type: Boolean, reflect: true})
+		["initially-hidden"]: boolean
 
 	firstUpdated() {
 		this["initially-hidden"] = false
 	}
+
+	@property({type: String})
+		error: string = "error"
+
+	@property({type: String})
+		loading: string = "loading"
+
+
+	//
+	// load and derived properties
+	//
+
+	@property({type: Boolean, reflect: true})
+		payload = false
+
+	@property({type: String, reflect: true})
+		state: string = "none"
+
+	private _load = loading.load<Payload>()
+
+	get load(): loading.Load<Payload> {
+		return this._load
+	}
+
+	set load(value: loading.Load<Payload>) {
+		if (!value) value = loading.none()
+		this._load = value
+		this.payload = !!loading.payload(value)
+		this.state = loading.LoadState[value.state].toLowerCase()
+		this.requestUpdate()
+	}
+
+	//
+	// overwritable rendering methods
+	//
 
 	renderNone(): TemplateResult {
 		return html`
@@ -61,6 +88,10 @@ export class IronLoading<Payload = any> extends LitElement {
 			<slot></slot>
 		`
 	}
+
+	//
+	// render each loading state
+	//
 
 	render() {
 		return loading.select(this.load, {
