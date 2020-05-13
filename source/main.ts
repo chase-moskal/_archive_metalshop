9c7347182fa75526f3c0647a9debf2bc5e568240
Chase Moskal <chasemoskal@gmail.com>
2020-05-12 19:34:39 -0700

    breaking: outsourced component registration
    the 'installMetalshop' function now returns the components object
    
    downstream devs can register the components themselves, apply their own
    mixins, styles, etc

--- source/main.ts
+++ source/main.ts
@@ -1,9 +1,17 @@
 
 import "menutown/dist/register-all.js"
 import {installMetalshop} from "./startup/install-metalshop.js"
+import {registerComponents} from "./toolbox/register-components.js"
+import {gatherDemoComponents} from "./startup/more/gather-demo-components.js"
 
 ~async function() {
-	const {supermodel, start} = await installMetalshop()
-	window["supermodel"] = supermodel
-	await start()
+	const metalshop = await installMetalshop()
+	window["metalshop"] = metalshop
+
+	registerComponents({
+		...metalshop.components,
+		...gatherDemoComponents(),
+	})
+
+	await metalshop.start()
 }()
--- source/startup/install-metalshop.ts
+++ source/startup/install-metalshop.ts
@@ -1,23 +1,18 @@
 
 import {MetalOptions} from "../interfaces.js"
-import {registerComponents} from "../toolbox/register-components.js"
-
 import {optionsFromDom} from "./more/options-from-dom.js"
 import {prepareSupermodel} from "./more/prepare-supermodel.js"
 import {wireComponentShares} from "./more/wire-component-shares.js"
-import {gatherDemoComponents} from "./more/gather-demo-components.js"
 
 export async function installMetalshop(options?: MetalOptions) {
 	options = options || await optionsFromDom("metal-config")
+
 	const supermodel = prepareSupermodel(options)
-	registerComponents({
-		...wireComponentShares(supermodel),
-		...gatherDemoComponents(),
-	})
-	return {
-		supermodel,
-		async start() {
-			await supermodel.auth.useExistingLogin()
-		}
+	const components = wireComponentShares(supermodel)
+
+	async function start() {
+		await supermodel.auth.useExistingLogin()
 	}
+
+	return {start, components, supermodel}
 }
